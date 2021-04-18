27dbb1595003e8e7ac5cb21e052a8acb765d3f83
Sanjay Pradeep <prasanth.n@yourdost.com>
2021-04-19 00:30:10 +0530

    first commit

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import firebase from 'firebase/app';
 import "firebase/database";
 
 let config = {
-    apiKey: "AIzaSyDNT2gxcTIRMVQQZ2bGLO8R9zWl1Sza8T8",
-    authDomain: "firecast-3f455.firebaseapp.com",
-    databaseURL: "https://firecast-3f455.firebaseio.com",
-    projectId: "firecast-3f455",
-    storageBucket: "firecast-3f455.appspot.com",
-    appId: "1:230208311970:android:1aaeb557b5c704a2",
+    apiKey: "XYZ",
+    authDomain: "XXX.firebaseapp.com",
+    databaseURL: "https://XXX.firebaseio.com",
+    projectId: "XXX",
+    storageBucket: "XXX.appspot.com",
+    appId: "ZZZ",
 };
 
 firebase.initializeApp(config);
